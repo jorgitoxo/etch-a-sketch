6af414134ca96d86d3cf98c88cf31aeb97716c48
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-10 21:35:59 +1300

    Add mouse trail effect
    Added padding to the container for even look of
    space
    Added event listener to container to handle color
    change on mouse hover
    Commented out container variable declaration

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,5 @@
 let gridSize = 16*16;
-let container = document.querySelector("#container");
-
+// const containers = document.querySelector("#containers");
 
 for (let i = 0; i < gridSize; i++) {
     let div = document.createElement("div");
@@ -8,3 +7,13 @@ for (let i = 0; i < gridSize; i++) {
     div.setAttribute('id', i+1);
     container.appendChild(div);
 }
+
+container.addEventListener("mouseover", (event) => {
+    let elementClass = event.target.getAttribute("class");
+    if (elementClass === "element") {
+        event.target.style.background = "#120055";
+        setTimeout(() => {
+            event.target.style.background = "";
+          }, 30000);
+    }
+},false,);
