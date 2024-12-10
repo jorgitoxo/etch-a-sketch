adb9e016c981adeb2c5a5a76135cf6ad47ff44ff
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-10 16:07:48 +1300

    Create HTML structure
    Created div container to hold elements
    Set grid size to 16
    Create and appeend element divs to fill the grid size

---
+++ javascript/main.js
@@ -0,0 +1,9 @@
+let gridSize = 16;
+let container = document.querySelector("#container");
+
+
+for (let i = 0; i < gridSize; i++) {
+    let div = document.createElement("div");
+    div.setAttribute('class', "element");
+    container.appendChild(div);
+}
