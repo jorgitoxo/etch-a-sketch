88c8efcc64356a0694e2c68c6c343b9ef81bd6b3
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-10 19:10:21 +1300

    Add element style
    Added style to container and elements to see a 16x16 grid
    Linked stylesheet in index file
    Added id attribute to elements in container div

--- javascript/main.js
+++ javascript/main.js
@@ -5,5 +5,6 @@ let container = document.querySelector("#container");
 for (let i = 0; i < gridSize; i++) {
     let div = document.createElement("div");
     div.setAttribute('class', "element");
+    div.setAttribute('id', i+1);
     container.appendChild(div);
 }
