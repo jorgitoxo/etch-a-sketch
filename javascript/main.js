f6fa12029dab22bb9544fefbece4bcc7872b946e
Jorge Leonard-Pinero <19485022+jorgitoxo@users.noreply.github.com>
2024-12-12 03:04:47 +1300

    Code refactor and cleanup
    Cleared previous version of funcions
    Replaced padWidth and padHeight variables with one variable padSize
    Added check so padSize is between 2 to 100 blocks
    Added button styling
    Adjusted width units

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,12 @@
 // Why does this work without the explicit declaration below ??
 // const container = document.querySelector("#container");
-// let contains = document.getElementById("container");
 
 function createPad(padSize) {
-    // try to create a div per row, then append the div row
-    // this way we should end up with a controllable number of rows
     for (let i = 0; i < padSize; i++) {
-        let div = document.createElement("div");
-        div.setAttribute('class', "element");
-        div.setAttribute('id', i+1);
-        container.appendChild(div);
-    }
-}
-
-function createPadv2(padWidth, padHeight) {
-    // try to create a div per row, then append the div row
-    // this way we should end up with a controllable number of rows
-    for (let i = 0; i < padHeight; i++) {
         let row = document.createElement("div");
         row.setAttribute('class', "row");
         row.setAttribute('id', i+1);
-        for (let j = 0; j < padWidth; j++) {
+        for (let j = 0; j < padSize; j++) {
             let item = document.createElement("div");
             item.setAttribute('class', "element");
             item.setAttribute('id', j+1);
@@ -31,39 +17,24 @@ function createPadv2(padWidth, padHeight) {
 }
 
 function resetPad(padSize) {
-    // Clear pad
-    // document.getElementById("container").innerHTML = "";
-    // document.getElementById("container").textContent = "";
+    // replaceChildren will remove elements if no parameter is passed
     document.getElementById("container").replaceChildren();
-
-    // Create new pad
     createPad(padSize);
 }
 
-function resetPadv2(padWidth, padHeight) {
-    // Clear pad
-    // document.getElementById("container").innerHTML = "";
-    // document.getElementById("container").textContent = "";
-    document.getElementById("container").replaceChildren();
-
-    // Create new pad
-    createPadv2(padWidth, padHeight);
-}
-
 function sketch () {
-    let padWidth = 16;
-    let padHeight = 16;
-    // let padSize = padWidth * padHeight;
-    createPadv2(padWidth, padHeight);
+    // --  explore turning padSize into arrow function expression --
+    let padSize = 16;
+    createPad(padSize);
 
     button.addEventListener('click', () => {
-        // pop up and capture new grid size dimensions
-        padWidth = parseInt(prompt("Enter pad width"));
-        padHeight = parseInt(prompt("Enter pad height"));
-        padSize = padWidth * padHeight;
-        
-        // call function to reset pad
-        resetPadv2(padWidth, padHeight);
+        do {
+            padSize = parseInt(prompt("Enter pad size in blocks"));
+            if ((padSize >= 2) && (padSize <= 100))
+                resetPad(padSize);
+            else
+                alert("Size limit 2-100 blocks!");
+        } while ((padSize < 2) || (padSize > 100));
     });
 
     container.addEventListener("mouseover", (event) => {
